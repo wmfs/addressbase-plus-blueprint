bf80fcc92babd8d1077dffc6339a9fbcb7c60317
Jez Higgins <jez@jezuk.co.uk>
2018-08-08 16:34:17 +0100

    refactor: Throw some describes into the test file

--- test/tests.js
+++ test/tests.js
@@ -9,10 +9,9 @@ const fs = require('fs')
 const rimraf = require('rimraf')
 const process = require('process')
 
-describe('Blueprint Tests', function () {
+describe('Addressbase Plus Tests', function () {
   this.timeout(process.env.TIMEOUT || 5000)
 
-  const STATE_MACHINE_NAME = 'ordnanceSurvey_importCsvFiles_1_0'
   const LIST_OF_CSV_SOURCE_FILES = [
     path.resolve(__dirname, './fixtures/sample-data/exeter-sample-data-20.csv')
   ]
@@ -29,145 +28,84 @@ describe('Blueprint Tests', function () {
       console.log(`Skipping tests due to unsafe PG_CONNECTION_STRING value (${process.env.PG_CONNECTION_STRING})`)
       this.skip()
     }
-  })
 
-  it('Should remove output directory ahead of tests, if it exists already', function (done) {
-    const outputPath = path.resolve(__dirname, './output')
-    if (fs.existsSync(outputPath)) {
-      rimraf(outputPath, {}, done)
-    } else {
-      done()
+    if (fs.existsSync(OUTPUT_AND_INPUT_DIR)) {
+      rimraf.sync(OUTPUT_AND_INPUT_DIR, {}, done)
     }
   })
 
-  it('should startup tymly so that we can test the blueprint', function (done) {
-    tymly.boot(
-      {
-        pluginPaths: [
-          require.resolve('@wmfs/tymly-etl-plugin'),
-          require.resolve('@wmfs/tymly-pg-plugin'),
-          path.resolve(__dirname, '../node_modules/@wmfs/tymly-test-helpers/plugins/allow-everything-rbac-plugin')
-        ],
+  describe('start up', () => {
+    it('start Tymly', function (done) {
+      tymly.boot(
+        {
+          pluginPaths: [
+            require.resolve('@wmfs/tymly-etl-plugin'),
+            require.resolve('@wmfs/tymly-pg-plugin'),
+            require.resolve('@wmfs/tymly-test-helpers/plugins/allow-everything-rbac-plugin')
+          ],
 
-        blueprintPaths: [
-          path.resolve(__dirname, './..')
-        ],
-
-        config: {}
-      },
-      function (err, tymlyServices) {
-        if (err) {
-          done(err)
-        }
-        tymlyService = tymlyServices.tymly
-        statebox = tymlyServices.statebox
-        client = tymlyServices.storage.client
-        done()
-      }
-    )
-  })
-
-  it('should execute the state-machine', function (done) {
-    statebox.startExecution(
-      {
-        sourceFilePaths: LIST_OF_CSV_SOURCE_FILES,
-        outputDirRootPath: OUTPUT_AND_INPUT_DIR,
-        sourceDir: OUTPUT_AND_INPUT_DIR
-      },
-      STATE_MACHINE_NAME,
-      {
-        sendResponse: 'COMPLETE'
-      },
-      function (err, executionDescription) {
-        expect(err).to.eql(null)
-        expect(executionDescription.status).to.eql('SUCCEEDED')
-        expect(executionDescription.stateMachineName).to.eql(STATE_MACHINE_NAME)
-        expect(executionDescription.currentStateName).to.eql('ImportingCsvFiles')
-        expect(executionDescription.ctx.sourceFilePaths).to.eql(LIST_OF_CSV_SOURCE_FILES)
-        expect(executionDescription.ctx.outputDirRootPath).to.eql(OUTPUT_AND_INPUT_DIR)
-        expect(executionDescription.ctx.sourceDir).to.eql(OUTPUT_AND_INPUT_DIR)
-        done()
-      }
-    )
-  })
-
-  it('should have created an output folder containing an upserts folder', function (done) {
-    fs.stat(OUTPUT_AND_INPUT_DIR, function (err, outputStats) {
-      if (err) {
-        console.log(OUTPUT_AND_INPUT_DIR, outputStats)
-        done(err)
-      } else {
-        fs.stat(UPSERTS_DIR, function (err, upsertsStats) {
+          blueprintPaths: [
+            path.resolve(__dirname, './..')
+          ]
+        },
+        function (err, tymlyServices) {
           if (err) {
-            console.log(UPSERTS_DIR, upsertsStats)
             done(err)
-          } else {
-            done()
           }
-        })
-      }
-    })
-  })
-
-  it('should have created a manifest.json file', function (done) {
-    fs.stat(MANIFEST_FILE, function (err, manifestStats) {
-      if (err) {
-        done(err)
-      } else {
-        // console.log(MANIFEST_FILE, manifestStats)
-        done()
-      }
-    })
-  })
-
-  it('should be a line count of 20 in the manifest file ', function (done) {
-    fs.readFile(MANIFEST_FILE, 'utf8', function (err, data) {
-      if (err) {
-        done(err)
-      } else {
-        let manifest = JSON.parse(data)
-        // console.log(JSON.stringify(manifest, null, 2))
-        expect(manifest.counts.byDir.upserts).to.eql(20)
-        done()
-      }
-    })
-  })
-
-  it('should have created an addressbase-holding.csv file', function (done) {
-    const CSV_FILE = path.resolve(UPSERTS_DIR, './addressbase-holding.csv')
-    fs.stat(CSV_FILE, function (err, csvStats) {
-      if (err) {
-        console.log(CSV_FILE, csvStats)
-        done(err)
-      } else {
-        done()
-      }
-    })
-  })
-
-  it('should return 20 rows when selecting from the newly populated table', function (done) {
-    client.query('SELECT uprn, hash_sum FROM ordnance_survey.addressbase_holding ORDER BY uprn ASC;', [],
-      function (err, result) {
-        if (err) {
-          done(err)
-        } else {
-          expect(result.rowCount).to.eql(20)
-          expect(result.rows[8].uprn).to.eql('100040214823')
+          tymlyService = tymlyServices.tymly
+          statebox = tymlyServices.statebox
+          client = tymlyServices.storage.client
           done()
         }
-      }
-    )
+      )
+    })
   })
 
-  it('Should remove output directory now tests are complete', function (done) {
-    if (fs.existsSync(OUTPUT_AND_INPUT_DIR)) {
-      rimraf(OUTPUT_AND_INPUT_DIR, {}, done)
-    } else {
-      done()
-    }
+  describe('import csv', () => {
+    it('execute the state-machine', async () => {
+      const executionDescription = await statebox.startExecution(
+        {
+          sourceFilePaths: LIST_OF_CSV_SOURCE_FILES,
+          outputDirRootPath: OUTPUT_AND_INPUT_DIR,
+          sourceDir: OUTPUT_AND_INPUT_DIR
+        },
+        'ordnanceSurvey_importCsvFiles_1_0',
+        {
+          sendResponse: 'COMPLETE'
+        }
+      )
+
+      expect(executionDescription.status).to.eql('SUCCEEDED')
+      expect(executionDescription.currentStateName).to.eql('ImportingCsvFiles')
+      expect(executionDescription.ctx.sourceFilePaths).to.eql(LIST_OF_CSV_SOURCE_FILES)
+      expect(executionDescription.ctx.outputDirRootPath).to.eql(OUTPUT_AND_INPUT_DIR)
+      expect(executionDescription.ctx.sourceDir).to.eql(OUTPUT_AND_INPUT_DIR)
+    })
+
+    it('check line count in the manifest file ', () => {
+      const data = fs.readFileSync(MANIFEST_FILE, 'utf8')
+
+      const manifest = JSON.parse(data)
+      // console.log(JSON.stringify(manifest, null, 2))
+      expect(manifest.counts.byDir.upserts).to.eql(20)
+    })
+
+    it('verify addressbase-holding.csv file', () => {
+      const CSV_FILE = path.resolve(UPSERTS_DIR, './addressbase-holding.csv')
+      fs.statSync(CSV_FILE)
+    })
+
+    it('verify table is populated', async () => {
+      const result = await client.query('SELECT uprn, hash_sum FROM ordnance_survey.addressbase_holding ORDER BY uprn ASC;')
+
+      expect(result.rowCount).to.eql(20)
+      expect(result.rows[8].uprn).to.eql('100040214823')
+    })
   })
 
-  it('should shutdown Tymly', () => {
-    return tymlyService.shutdown()
+  describe('clean up', () => {
+    it('shutdown Tymly', () => {
+      return tymlyService.shutdown()
+    })
   })
 })
