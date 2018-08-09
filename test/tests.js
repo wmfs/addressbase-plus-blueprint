424fb91eed9ea9aa046bfcc1ac178cbf59d60242
Jez Higgins <jez@jezuk.co.uk>
2018-08-09 10:08:39 +0100

    feat: Move Addressbase Plus sync into this blueprint from Addressbox

--- test/tests.js
+++ test/tests.js
@@ -15,9 +15,12 @@ describe('Addressbase Plus Tests', function () {
   const LIST_OF_CSV_SOURCE_FILES = [
     path.resolve(__dirname, './fixtures/sample-data/exeter-sample-data-20.csv')
   ]
-  const OUTPUT_AND_INPUT_DIR = path.resolve(__dirname, './output')
-  const UPSERTS_DIR = path.resolve(OUTPUT_AND_INPUT_DIR, './upserts')
-  const MANIFEST_FILE = path.resolve(OUTPUT_AND_INPUT_DIR, './manifest.json')
+  const outputDir = path.resolve(__dirname, './output')
+  const upsertsDir = path.resolve(outputDir, './upserts')
+  const MANIFEST_FILE = path.resolve(outputDir, './manifest.json')
+
+  const syncOutputDir = path.resolve(outputDir, 'sync')
+
 
   let tymlyService
   let statebox
@@ -29,8 +32,8 @@ describe('Addressbase Plus Tests', function () {
       this.skip()
     }
 
-    if (fs.existsSync(OUTPUT_AND_INPUT_DIR)) {
-      rimraf.sync(OUTPUT_AND_INPUT_DIR, {}, done)
+    if (fs.existsSync(outputDir)) {
+      rimraf.sync(outputDir, {})
     }
   })
 
@@ -45,7 +48,8 @@ describe('Addressbase Plus Tests', function () {
           ],
 
           blueprintPaths: [
-            path.resolve(__dirname, './..')
+            path.resolve(__dirname, './..'),
+            require.resolve('@wmfs/gazetteer-blueprint')
           ]
         },
         function (err, tymlyServices) {
@@ -66,8 +70,8 @@ describe('Addressbase Plus Tests', function () {
       const executionDescription = await statebox.startExecution(
         {
           sourceFilePaths: LIST_OF_CSV_SOURCE_FILES,
-          outputDirRootPath: OUTPUT_AND_INPUT_DIR,
-          sourceDir: OUTPUT_AND_INPUT_DIR
+          outputDirRootPath: outputDir,
+          sourceDir: outputDir
         },
         'ordnanceSurvey_importCsvFiles_1_0',
         {
@@ -78,8 +82,8 @@ describe('Addressbase Plus Tests', function () {
       expect(executionDescription.status).to.eql('SUCCEEDED')
       expect(executionDescription.currentStateName).to.eql('ImportingCsvFiles')
       expect(executionDescription.ctx.sourceFilePaths).to.eql(LIST_OF_CSV_SOURCE_FILES)
-      expect(executionDescription.ctx.outputDirRootPath).to.eql(OUTPUT_AND_INPUT_DIR)
-      expect(executionDescription.ctx.sourceDir).to.eql(OUTPUT_AND_INPUT_DIR)
+      expect(executionDescription.ctx.outputDirRootPath).to.eql(outputDir)
+      expect(executionDescription.ctx.sourceDir).to.eql(outputDir)
     })
 
     it('check line count in the manifest file ', () => {
@@ -91,7 +95,7 @@ describe('Addressbase Plus Tests', function () {
     })
 
     it('verify addressbase-holding.csv file', () => {
-      const CSV_FILE = path.resolve(UPSERTS_DIR, './addressbase-holding.csv')
+      const CSV_FILE = path.resolve(upsertsDir, './addressbase-holding.csv')
       fs.statSync(CSV_FILE)
     })
 
@@ -103,7 +107,62 @@ describe('Addressbase Plus Tests', function () {
     })
   })
 
+  describe('synchronization', () => {
+    it('populate test data', async () => {
+      const sqlFile = path.resolve(__dirname, './fixtures/scripts/wmfs-gazetteer-setup.sql')
+      await client.runFile(sqlFile)
+    })
+
+    it('check for a single gazetteer record', async () => {
+      const result = await client.query('SELECT count(uprn) FROM wmfs.gazetteer')
+      const count = result.rows[0].count
+      expect(count).to.eql('1')
+    })
+
+    let existing
+    it('grab existing row', async () => {
+      const result = await client.query('SELECT * from wmfs.gazetteer')
+      expect(result.rowCount).to.eql(1)
+
+      existing = result.rows[0]
+    })
+
+    it('run synchronize-addressbox-premium state machine', async () => {
+      const executionDescription = await statebox.startExecution(
+        {
+          outputDir: syncOutputDir
+        }, // input
+        'ordnanceSurvey_synchronizeAddressbasePlus_1_0', // state machine name
+        {
+          sendResponse: 'COMPLETE'
+        }
+      )
+
+      expect(executionDescription.status).to.eql('SUCCEEDED')
+      expect(executionDescription.currentStateName).to.eql('SynchronizingTable')
+      expect(executionDescription.ctx.outputDir).to.eql(syncOutputDir)
+    })
+
+    it('check the newly populated gazetteer table', async () => {
+      const result = await client.query('SELECT uprn FROM wmfs.gazetteer ORDER BY uprn ASC;', [])
+      expect(result.rowCount).to.eql(21)
+    })
+
+    it('pre-existing row is unchanged', async () => {
+      const result = await client.query(`SELECT * from wmfs.gazetteer WHERE uprn = ${existing.uprn}`)
+      expect(result.rowCount).to.eql(1)
+
+      expect(result.rows[0]).to.eql(existing)
+    })
+  })
+
+
   describe('clean up', () => {
+    it('clear down database', async () => {
+      await client.query('DROP SCHEMA ordnance_survey CASCADE')
+      await client.query('DROP SCHEMA wmfs CASCADE')
+    })
+
     it('shutdown Tymly', () => {
       return tymlyService.shutdown()
     })
